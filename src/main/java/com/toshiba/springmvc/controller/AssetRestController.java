9334f430874a3b9a97950e37caa01ed26977ecf2
prashdeep <pradeep.kumar44@gmail.com>
2019-11-21 14:53:44 +0530

    committing the servlet xml file

--- src/main/java/com/toshiba/springmvc/controller/AssetRestController.java
+++ src/main/java/com/toshiba/springmvc/controller/AssetRestController.java
@@ -1,7 +1,47 @@
 package com.toshiba.springmvc.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.toshiba.springmvc.model.Asset;
+import com.toshiba.springmvc.service.AssetService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/assets")
 public class AssetRestController {
+
+    private final AssetService assetService;
+
+    public AssetRestController(AssetService assetService) {
+        this.assetService = assetService;
+    }
+
+    @GetMapping
+    public String fetchAllAssets(Model model){
+        List<Asset> assetList = this.assetService.listAll();
+        model.addAttribute("assets", assetList);
+        return "assets";
+    }
+
+    @GetMapping("/{id}")
+    public String fetchAssetById(@PathVariable("id") long assetId,  Model model){
+        Asset asset = this.assetService.findByAssetId(assetId);
+        model.addAttribute("asset", asset);
+        return "asset-details";
+    }
+
+    @PostMapping
+    public String saveAsset(Asset asset){
+        this.assetService.saveAsset(asset);
+        return "redirect:/assets";
+    }
+
+    @PutMapping("/{id}")
+    public String updateAsset(@PathVariable long id, Asset asset, Model model){
+        this.assetService.updateAsset(id, asset);
+        return "redirect:/assets";
+    }
+
+
 }
