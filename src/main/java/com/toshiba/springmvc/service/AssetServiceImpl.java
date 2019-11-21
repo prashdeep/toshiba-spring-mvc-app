b44de7181088fdcee3eda41a4f1c8a0c6cb6ab7b
prashdeep <pradeep.kumar44@gmail.com>
2019-11-21 12:54:54 +0530

    daoimpl and service impl completed

--- src/main/java/com/toshiba/springmvc/service/AssetServiceImpl.java
+++ src/main/java/com/toshiba/springmvc/service/AssetServiceImpl.java
@@ -1,7 +1,10 @@
 package com.toshiba.springmvc.service;
 
 import com.toshiba.springmvc.dao.AssetDAO;
+import com.toshiba.springmvc.model.Asset;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AssetServiceImpl implements AssetService {
@@ -10,5 +13,30 @@ public class AssetServiceImpl implements AssetService {
 
     public AssetServiceImpl(AssetDAO assetDAO) {
         this.assetDAO = assetDAO;
+    }
+
+    @Override
+    public Asset saveAsset(Asset asset) {
+        return assetDAO.saveAsset(asset);
+    }
+
+    @Override
+    public void updateAsset(long assetId, Asset asset) {
+        assetDAO.updateAsset(assetId,asset);
+    }
+
+    @Override
+    public void deleteAsset(long assetId) {
+        this.assetDAO.deleteAsset(assetId);
+    }
+
+    @Override
+    public List<Asset> listAll() {
+        return this.assetDAO.listAll();
+    }
+
+    @Override
+    public Asset findByAssetId(long assetId) {
+        return this.assetDAO.findByAssetId(assetId);
     }
 }
